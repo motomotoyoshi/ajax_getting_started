81ec5b89e8fc2c1378bc13eeedd2a68afa40470f
motomotoyoshi <motomoto.git@gmail.com>
2021-09-11 18:40:15 +0900

    jsonファイルの読み書きに変更

--- test.php
+++ test.php
@@ -1,11 +1,30 @@
 <?php
+$filename= 'a.json';
 $name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
-
 $computedString = "Hi, ". $name;
-
 $array = [
     'userName' => $name,
     'computedString' => $computedString
 ];
+if(is_readable($filename)){
 
-echo json_encode($array);
+    $data = json_decode(mb_convert_encoding(file_get_contents($filename), 'UTF-8',  'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'), true);
+    
+    if($data['computedString'] == $computedString) {
+        echo json_encode($data);
+    } else {
+        if (is_writeable($filename)) {
+            $data['userName'] = $name;
+            $data['computedString'] = $computedString;
+
+            $handle = fopen($filename, "w");
+            $filesize = filesize($filename);
+            fwrite($handle, json_encode($data));
+            fclose($handle);
+            
+            echo json_encode($data);
+        } else {
+            echo json_encode("not writeable.");
+        }
+    }
+}
