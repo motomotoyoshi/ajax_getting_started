6e39306a32afce442a717e4e90c9c11e58bfe048
motomotoyoshi <motomoto.git@gmail.com>
2021-09-11 20:08:43 +0900

    PHP処理の簡略化

--- test.php
+++ test.php
@@ -2,29 +2,15 @@
 $filename= 'data.json';
 $name = !empty($_POST['userName']) ? $_POST['userName'] : 'no name';
 $computedString = "Hi, ". $name;
-$array = [
-    'userName' => $name,
-    'computedString' => $computedString
-];
-if(is_readable($filename)){
 
-    $data = json_decode(mb_convert_encoding(file_get_contents($filename), 'UTF-8',  'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN'), true);
-    
-    if($data['computedString'] == $computedString) {
-        echo json_encode($data);
-    } else {
-        if (is_writeable($filename)) {
-            $data['userName'] = $name;
-            $data['computedString'] = $computedString;
+if (is_writeable($filename)) {
+    $data['userName'] = $name;
+    $data['computedString'] = $computedString;
 
-            $handle = fopen($filename, "w");
-            $filesize = filesize($filename);
-            fwrite($handle, json_encode($data));
-            fclose($handle);
-            
-            echo json_encode($data);
-        } else {
-            echo json_encode("not writeable.");
-        }
-    }
+    $handle = fopen($filename, "w");
+    $filesize = filesize($filename);
+    fwrite($handle, json_encode($data));
+    fclose($handle);
+
+    echo json_encode($data);
 }
